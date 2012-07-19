223438bfc3eefb06b847b2c7a9b98892e9e4247b
Yehuda Katz <wycats@gmail.com>
2012-07-18 21:34:56 -0700

    Add an ember-data lifecycle guide

--- edge_template.rb
+++ edge_template.rb
@@ -12,7 +12,8 @@ run "rm public/index.html"
 # Install required gems
 gem "active_model_serializers"
 gem_group :assets do
-  gem "ember-rails", github: "emberjs/ember-rails"
+  gem "ember-rails", path: "~/Code/ember-rails"
+  #gem "ember-rails", github: "emberjs/ember-rails"
 end
 
 run "bundle install"
