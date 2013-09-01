6cc54a1f001da5f9fead4592a9c1a5d68116c4b8
Tom Dale <tom@tomdale.net>
2013-09-01 01:07:23 -0700

    Actually minification is still broken

--- javascripts/app/about/slider.js
+++ javascripts/app/about/slider.js
@@ -1,1 +1,13 @@
-$(function(){$("#slider").bxSlider({displaySlideQty:4,moveSlideQty:4,auto:!0,pager:!0,speed:700,pause:5e3}),$(".bx-wrapper, .bx-window").css("width",940)});
+$(function() {
+  $('#slider').bxSlider({
+    displaySlideQty: 4,
+    moveSlideQty: 4,
+    auto: true,
+    pager: true,
+    speed: 700,
+    pause: 5000
+  });
+
+  // Hack to force proper sizing
+  $('.bx-wrapper, .bx-window').css('width', 940);
+});
